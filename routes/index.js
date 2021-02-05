efe4b6624a6c12137b85283a7326ce22658f3ea7
sholdahl <sam.holdahl@gmail.com>
2021-02-04 18:57:34 -0600

    replaced layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Sam's Site "});
+  res.redirect('/catalog');
 });
 
 module.exports = router;
