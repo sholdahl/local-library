f5b96081a4ae3ccb7bb9e72e4cb9d6ffd4f89e12
sholdahl <sam.holdahl@gmail.com>
2021-02-09 10:33:57 -0600

    final version before pushing to heroku

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ dotenv.config();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://sholdahl:secretpassword@cluster0.smkcs.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url ='mongodb+srv://sholdahl:secretpassword@cluster0.smkcs.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
